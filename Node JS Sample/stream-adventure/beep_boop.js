b97f0c65f0fb1314186f46301fef5f379c778500
Daniel Cruger <danielcruger@danielcruger.com>
2018-03-21 16:28:14 -0400

    Stream-Adventure: Completed through lesson 5

--- Node JS Sample/stream-adventure/beep_boop.js
+++ Node JS Sample/stream-adventure/beep_boop.js
@@ -1,6 +1,6 @@
 // Lesson 1
 var fs = require("fs")
-var file = process.argv[2]
+//var file = process.argv[2]
 //fs.createReadStream(process.stdin).pipe(process.stdout)
 
 //Lesson 2
@@ -22,6 +22,7 @@ process.stdin.on('end', function () {
 */
 
 //Lesson 4
+/*
 var body = ''
 process.stdin.on('readable', function () {
   var chunk = process.stdin.read();
@@ -31,11 +32,40 @@ process.stdin.on('readable', function () {
 });
 
 process.stdin.on('end', function () {
-  var output = body.split('\n');
-  for(var i=0;i<output.length;i++)
-    if(i % 2 == 0)
-      process.stdout.write(output[i].toLowerCase() + "\n");
-    else
-      process.stdout.write(output[i].toUpperCase() + "\n");
+  var output = body.split('');
+  output = output.reverse();
+  output = output.join('')
+  process.stdout.write(output);
   process.stdout.write("\n");
 });
+*/
+
+//Lesson 5
+var http = require('http')
+var fs = require('fs')
+var port = process.argv[2];
+
+var server = http.createServer(function (req, resp){
+  var body = '';
+  req.setEncoding('utf8');
+  if(req.method === 'POST') {
+    req.on('data', (chunk) => {
+      body += chunk;
+    });
+    req.on('end', () => {
+    try {
+      var data = body.toUpperCase();
+      // write back something interesting to the user:
+      resp.write(data);
+      resp.end();
+    } catch (er) {
+      // uh oh! bad json!
+      resp.statusCode = 400;
+      return resp.end(`error: ${er.message}`);
+    }
+    });
+  }
+}).on('error', function(err) {
+  console.log(err)
+})
+server.listen(port)
